f5716ee629351d6e725e08329ab8ca49a353b02d
yasserali <alali.yasser@protonmail.com>
2021-04-29 11:35:47 +0300

    simple calculator using a function

---
+++ functions.py
@@ -0,0 +1,8 @@
+def simple_calculator(num_one, num_two):
+    add = num_one + num_two
+    multiply = num_one * num_two
+    sub = num_one - num_two
+    div = num_one / num_two
+    return add, multiply, sub, div
+
+print(simple_calculator(10, 2))
